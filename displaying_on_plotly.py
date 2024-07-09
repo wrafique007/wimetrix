d5cc84bbb43a204c3969ec03798eaa04f7dc2e25
wrafique007 <46618628+wrafique007@users.noreply.github.com>
2024-07-09 14:58:05 +0500

    Add files via upload

---
+++ displaying_on_plotly.py
@@ -0,0 +1,40 @@
+import chart_studio.plotly as py
+from plotly.offline import iplot
+import plotly.graph_objs as go
+import plotly.figure_factory as FF
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('image_content.csv', sep=',')
+
+print(df)
+
+layout = go.Layout(
+    autosize=False,
+    width=2000,
+    height=2000,
+    xaxis=go.layout.XAxis(linecolor="black", linewidth=1, mirror=True),
+    yaxis=go.layout.YAxis(linecolor="black", linewidth=1, mirror=True),
+    margin=go.layout.Margin(l=75, r=75, b=125, t=125, pad=4),
+)
+
+column_widths = [150, 50, 150, 50, 50, 50, 50, 50, 50,
+                 50, 50, 50, 150, 150, 50, 50, 50, 50]
+
+#layout = dict(autosize=True)
+image_content = FF.create_table(df, height_constant=100, index=False, )
+fig = go.Figure(data=image_content, layout=layout)
+fig.layout.width = 3000
+
+f2 = go.FigureWidget(fig)
+f2.show()
+#fig.show()
+#iplot(fig)
+#py.iplot(image_content, filename='image_content-table')
+""" fig = go.Figure(data=[go.Table(
+    columnwidth=column_widths,
+    header=dict(values = df.columns), 
+    cells=dict(values=df.iloc[0:]))
+])
+fig.show()  """
---
+++ reading_from_a_json_file.py
@@ -0,0 +1,53 @@
+
+import pprint
+import json
+import csv
+
+json_data = None
+with open('image_content.json', 'r') as f:
+    data = f.read()
+    json_data = json.loads(data)
+
+# print json to screen with human-friendly formatting
+#pprint.pprint(json_data['content'], compact=True)
+
+split_json_data = json_data['content'].split('\n')
+
+
+# for count in range(len(split_json_data)):
+#     print(split_json_data[count])
+
+# headers = split_json_data[1].split(',')
+# data = split_json_data[2].split(',')
+# data1 = split_json_data[3].split(',')
+# data2 = split_json_data[4].split(',')
+# data3 = split_json_data[5].split(',')
+
+# print(headers)
+# print(data)
+# print(data1)
+# print(data2)
+# print(data3)
+
+
+csv_file = open('image_content.csv', 'w')
+csv_writer = csv.writer(csv_file)
+
+for count in range(0,len(split_json_data)):
+    if count != 1:
+        if count == 0:
+            header = split_json_data[count].split('|')
+            # remove white spaces
+            for c in range(len(header)):
+                header[c] = header[c].strip(' ')
+            csv_writer.writerow(header)
+        else:
+            row = split_json_data[count].split('|')
+            # remove white spaces
+            for c in range(len(row)):
+                row[c] = row[c].strip(' ')
+            csv_writer.writerow(row)
+
+csv_file.close()
+
+
